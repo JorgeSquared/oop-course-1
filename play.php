c56b76a7d39a1f3861eab6574ee1a53fe8ceef0b
jirka <jorgetimes2@gmail.com>
2022-05-03 15:17:21 +0200

    Methods that do work

--- play.php
+++ play.php
@@ -19,6 +19,28 @@ class Ship
     {
         return $this->name;
     }
+
+    public function getNameAndSpec($useShortFormat)
+    {
+        if ($useShortFormat) {
+            return sprintf(
+                '%s: %s/%s/%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        } else {
+            return sprintf(
+                '%s: w:%s j:%s s:%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+
+    }
 }
 
 $myShip = new Ship();
@@ -31,4 +53,6 @@ $myShip->sayHello();
 echo '<hr/>';
 echo $myShip->getName();
 echo '<hr/>';
-var_dump($myShip->weaponPower);
+echo $myShip->getNameAndSpec(false);
+echo '<hr/>';
+echo $myShip->getNameAndSpec(true);
